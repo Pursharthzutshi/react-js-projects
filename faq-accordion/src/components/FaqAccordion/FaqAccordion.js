6799c1b13534865a43cb501da46142fe8d0a23f3
Pursharth Zutshi <pursharthzutshi@gmail.com>
2022-10-01 22:36:12 +0530

    updating adding faq-accordion

--- faq-accordion/src/components/FaqAccordion/FaqAccordion.js
+++ faq-accordion/src/components/FaqAccordion/FaqAccordion.js
@@ -7,6 +7,8 @@ function App() {
   // const [loading,setLoading] = useState(false);
   const [showAns,setShowAns] = useState(false);
   const [changeIcon,setChangeIcon] = useState(true);
+  const [knowMoreBox,setKnowMoreBox] = useState();
+  const [backgroundImg,setBackgroundImg] = useState(false);
 
   function showAnsBox(e){
     console.log(showAns)
@@ -20,7 +22,22 @@ function App() {
      setChangeIcon(!changeIcon)
   }
 
+  function showKnowMoreBox(e){
+    setKnowMoreBox(!knowMoreBox)
+  if(knowMoreBox){
+    return (setKnowMoreBox(false),setBackgroundImg(false))
+  }
+  setKnowMoreBox(e)
+  setBackgroundImg(true)
+  }
 
+  function closeAll(){
+    
+    setKnowMoreBox(!knowMoreBox)  
+    setBackgroundImg(!backgroundImg)
+
+  }
+  
   return (
     <div className='main-faq-container'>
     <div className='faq-container'>
@@ -37,14 +54,28 @@ function App() {
               <p>{val.ques}</p>
               <button onClick={()=>{showAnsBox(val.ques)}}>{changeIcon ?"+":"-"}</button>
               </div>
-              {showAns === val.ques && <p>{val.ans}</p>}
-    
+              {showAns === val.ques && <p>{val.ans}<a href="#" onClick={()=>{showKnowMoreBox(val.ques)}}>Know More</a></p>}
+
+              {knowMoreBox === val.ques && 
+              <div className="know-more-box-container">
+              <div className="know-more-box">
+                <div className="cross-sign-div">
+                <p onClick={closeAll}>❌</p>
+                </div>
+                <h2>Know More</h2>
+                <p className="know-more-desc">{val.desc}</p>
+                <button className="know-more-button">Visit Help</button>
+                </div>
+                </div>
+              }
+            </div>
             
             </div>
-            </div>
+            
             )
           })
         }
+              {backgroundImg && <div className="bg-div"></div>}
 
       </div>
     </div>    
--- faq-accordion/src/components/FaqAccordion/FaqAccordionData.js
+++ faq-accordion/src/components/FaqAccordion/FaqAccordionData.js
@@ -3,41 +3,50 @@ const FaqAccordionData = [
         id:1,
         ques:"Payment?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
+        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed sapien non mi blandit vulputate ac porttitor nisl. Sed accumsan nulla mattis, egestas sem sed, malesuada purus. Aliquam erat volutpat. Duis vitae sem at dolor feugiat semper vitae id diam. Praesent finibus sem eget efficitur pretium. Pellentesque sollicitudin nunc nec ligula vestibulum commodo. Aenean a rhoncus leo, luctus rutrum nunc. Aenean vel dolor tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti. Vivamus eu velit dui. Sed euismod luctus lorem, vel mollis massa tempus ac. Integer sed feugiat erat, eget elementum ex.nt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis cond"
     },
     {
         id:2,
         ques:"Account ?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
-    },
+        desc:"Quisque iaculis tincidunt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis condimentum quam."
+     },
     {
         id:3,
         ques:"Profile?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
+        desc:"Quisque iaculis tincidunt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis condimentum quam."
+
     },
     {
         id:4,
         ques:"Return Policy ?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
+        desc:"Quisque iaculis tincidunt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis condimentum quam."
     },
     {
         id:5,
         ques:"Refund Policy ?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
+        desc:"Quisque iaculis tincidunt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis condimentum quam."
     },
     {
         id:6,
         ques:"Shipping Options ?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
+        desc:"Quisque iaculis tincidunt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis condimentum quam."
     },
     {
         id:7,
         ques:"Your Orders ?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
+        desc:"Quisque iaculis tincidunt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis condimentum quam."
     },
     {
         id:8,
         ques:"Loremasd ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque suscipit dui sed volutpat cursus. ?",
         ans:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu diam eleifend, pharetra mauris eget, gravida diam. Suspendisse eget pulvinar libero. Nullam sed lectus sit amet orci cursus pharetra non in arcu. Aenean feugiat rhoncus tempor. Mauris tincidunt, sem ut ornare pharetra, lectus mauris egestas velit, et scelerisque leo lorem id dui. ",
+        desc:"Quisque iaculis tincidunt leo quis dapibus. Phasellus vehicula consequat ullamcorper. Sed in eros ligula. Morbi iaculis ligula id diam vehicula, ac scelerisque ipsum mollis. Cras eget suscipit ligula, in ullamcorper orci. Nullam aliquam mi at euismod pharetra. Pellentesque id dolor sodales, porta ligula quis, finibus enim. Cras vel tincidunt eros, vel tincidunt leo. Quisque condimentum est sed risus ornare, lacinia faucibus tortor vehicula. Morbi non neque tortor. Sed risus diam, maximus nec dui ac, aliquet vehicula sapien. Morbi quis condimentum quam."
     },
     
 ]
